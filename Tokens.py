248398445ba1e976b6e473507d742b33e0fbdc85
pbhanot1 <priya_bhanot@brown.edu>
2023-05-09 04:42:57 -0400

    display screen setup

--- Tokens.py
+++ Tokens.py
@@ -5,7 +5,7 @@ import pygame_gui
 pygame.init()
 screen = pygame.display.set_mode((1200, 900))
 class Food(object):
-
+# tutorial used for counter: https://opensource.com/article/20/1/add-scorekeeping-your-python-game
     def __init__(self,food_screen):
         self.image= pygame.transform.scale(pygame.image.load("treatbone.png").convert(),(40,20))
         self.food_screen=food_screen
